1b6f91d1bfc810a78238181134fc5f9d989f9520
marwan-9 <marwan.ahmed011@eng-st.cu.edu.eg>
2024-03-12 22:59:23 +0200

    Prettifiy console output, adding gitignore

--- src/reportEngine.js
+++ src/reportEngine.js
@@ -1,15 +1,15 @@
 function generateReport(matches, filePath) {
-    console.log(`\nFile: ${filePath}`);
+    console.log('\x1b[36m%s\x1b[0m',`\nFile: ${filePath}`);
     if (matches.length === 0) {
-        console.log("No vulnerabilities found.");
+        console.log('\x1b[32m%s\x1b[0m',"No vulnerabilities found.");
     } else {
-        console.log("Vulnerabilities found:");
+        console.log('\x1b[31m%s\x1b[0m',"Vulnerabilities found:");
         matches.forEach((match, index) => {
-            console.log(`#${index + 1}:`);
-            console.log(`Rule: ${match.ruleName}`);
-            console.log(`Description: ${match.description}`);
-            console.log(`Location: Start ${match.location.start}, End ${match.location.end}`);
-            console.log(`Line: ${match.line}`);
+            console.log('\x1b[36m%s\x1b[0m',`#${index + 1}:`);
+            console.log('\x1b[31m%s\x1b[0m',`Rule: ${match.ruleName}`);
+            console.log('\x1b[33m%s\x1b[0m',`Description: ${match.description}`);
+            console.log('\x1b[33m%s\x1b[0m',`Location: Start ${match.location.start}, End ${match.location.end}`);
+            console.log('\x1b[33m%s\x1b[0m',`Line: ${match.line}`);
             console.log("------------");
         });
     }
