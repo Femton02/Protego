ac4581797fb3e3e3f5a83273aef1cbc55c3fe506
Anas Sherif <96656458+Femton02@users.noreply.github.com>
2024-05-05 05:30:46 +0300

    Added the handling ellipsis function in the tree_sitter_query_tree to handle the first kind of ellipsis which is function call/method

--- src/core/rule_parser.py
+++ src/core/rule_parser.py
@@ -1,16 +1,27 @@
 from t_sitter.tree_sitter_utils import parse_js_code, traverse_tree, visualize_tree, query_tree, Node
+from tree_sitter_query_tree import QTNode, TSQueryTree
 import uuid
 import re
 
+#____________________________________________________________________________________#
+#                                 HELPER FUNCTIONS
+#____________________________________________________________________________________#   
+
+
 def rule_preprocessing(rule: str) -> tuple[str, dict]:
-    regexpattern = r'(\$<(\w+)>)'
-    variables = {}
+    variablepattern = r'(\$<(\w+)>)'        #pattern to catch variables
+    ellipsispattern = r'(\$<(\.\.\.)>)'     #pattern to catch ellipsis
+    catches = {}
     uuidobj = uuid.uuid4()
     uuidstr = str(uuidobj.int)[0:38] #to ensure fixed length
-    for match in re.finditer(regexpattern, rule):
-        variables[match.group(2)] = uuidstr
-    outputstr = re.sub(regexpattern, "_"+ uuidstr +r'\2', rule)
-    return outputstr, variables
+    for match in re.finditer(variablepattern, rule):
+        catches[match.group(2)] = uuidstr
+    outputstr = re.sub(variablepattern, "_"+ uuidstr +r'\2', rule)
+
+    for match in re.finditer(ellipsispattern, outputstr):
+        catches["ELLIPSIS"] = uuidstr
+    outputstr = re.sub(ellipsispattern, "_"+ uuidstr + "ELLIPSIS", outputstr) #replace ellipsis with a unique string
+    return outputstr, catches
 
 def get_first_multi_children_node(tree):
     last_node = None
@@ -20,56 +31,25 @@ def get_first_multi_children_node(tree):
         last_node = node
     return last_node
 
-def traverse_anas(node: Node, counter: int,  catch: dict, variables: dict, content: dict, level: int = 1) -> tuple[str, int, dict, dict, dict]:
-    # create string "(node.type + all my children's strings)"
-    query = "(" + node.type + "\n"
-    for i in range(level):
-        query += "    "
-    for child in node.named_children:
-        if child == node.named_child(0):
-            query += " ."
-        concatquery, counter, _, _, _ = traverse_anas(child, counter, catch, variables, content, level+1)
-        query += concatquery
-        query += " ."
-    query += "\n"
-    for i in range(level-1):
-        query += "    "
-    query += ")"
-    if (node.child_count < 1):
-        if(len(node.text.decode()) > 39): #check if length is bigger than 39 (uuid length)
-            if(node.text.decode()[39:] in catch and node.text.decode()[39:] != "_"): #if it is in variables dict and is named
-                if(catch[node.text.decode()[39:]] == node.text.decode()[1:39]): #if the uuid matches then add it
-                    query += "@param" + str(counter)
-                    variables[counter] = node.text.decode()[39:]
-                    counter += 1
-        else: #the size is less than 39 then it is literal match
-            query += "@param" + str(counter)
-            content[counter] = node.text.decode()
-            counter += 1
-    query += "\n"
-    for i in range(level-1):
-        query += "    "
-    return query, counter, catch, variables, content
-    
+
+#____________________________________________________________________________________#
+#                                   ENTRY POINT
+#____________________________________________________________________________________#   
 
 def parse_rule(rule: str) -> tuple[str, dict, dict]:
     preprocessedrule, catch = rule_preprocessing(rule)
     tree = parse_js_code(preprocessedrule)
     root = get_first_multi_children_node(tree)
-    visualize_tree(tree)
-    variables = {}
-    content = {}
-    query, _, _, variables, content = traverse_anas(root, 1, catch, variables, content)
-
+    tsqt = TSQueryTree(root, catch)
+    tsqt.build_query_tree(root, tsqt.root)
+    tsqt.handle_ellipsis(tsqt.root)
+    query = tsqt.build_query_str(tsqt.root) + "@content"
+    variables = tsqt.variables
+    content = tsqt.content
+    tsqt.visualize_query_tree()
     return query, variables, content
 
-query, var, cont = parse_rule("x = 1;_;x = 3;")
+query, var, cont = parse_rule("func($<...>, $<zzz>, $<...>)")
 print(query)
 print(var)
 print(cont)
-
-
-# <Node type=import_statement, start_point=(0, 0), end_point=(0, 40)>
-# <Node type="import", start_point=(0, 0), end_point=(0, 6)>
-# <Node type=import_clause, start_point=(0, 7), end_point=(0, 20)>
-# <Node type=named_imports, start_point=(0, 7), end_point=(0, 20)>
--- src/core/tree_sitter_query_tree.py
+++ src/core/tree_sitter_query_tree.py
@@ -1,4 +1,5 @@
 from t_sitter.tree_sitter_utils import parse_js_code, traverse_tree, visualize_tree, query_tree, Node, Tree
+
 import graphviz
 import random
 
@@ -7,10 +8,11 @@ class QTNode:
     This class is used to represent a node in the query graph.
     """
     
-    def __init__(self, value: str):
-        self.id = random.randint(0, 100000)
+    def __init__(self, value: str, text: str = None):
+        self.id = random.randint(0, 100000) # random id for visualization
         self.child_index = None
         self.value = value
+        self.text = text
         self.children = []
         self.parent = None
     
@@ -43,6 +45,18 @@ class QTNode:
         """
         return self.parent
     
+    def delete_child_index(self, index: int):
+        """
+        This function deletes the child at the given index.
+        
+        Args:
+            index (int): The index of the child
+        """
+        self.children[index].parent = None
+        self.children.pop(index)
+
+
+    
     
     def __str__(self):
         return f"Node({self.child_index}, {self.value}, {self.children})"
@@ -52,25 +66,31 @@ class TSQueryTree:
     This class is used to represent the query for a tree-sitter as a tree.
     """
     
-    def __init__(self, root: Node):
+    def __init__(self, root: Node, catch: dict = {}):
         self.TS_root = root
-        self.root: QTNode = QTNode("root")
+        self.root: QTNode = QTNode("", root.text.decode())
         self.query_str = ""
-    
-    def build_query_tree(self, node: Node, parent: QTNode):
+        self.counter = 1
+        self.catch = catch
+        self.variables = {}
+        self.content = {}
+
+    def get_child_index_from_value(self, node: QTNode) -> int:
         """
-        This function builds the query tree from the TS_root.
-        """
-        curr_node = QTNode("(" + node.type)
-        parent.add_child(curr_node)
-        parent = curr_node
-        for child in node.named_children:
-            if child == node.named_child(0):
-                parent.add_child(QTNode("."))
-            self.build_query_tree(child, curr_node)
-            parent.add_child(QTNode("."))
+        This function gets the index of the child with the given value.
         
-        parent.add_child(QTNode(")"))
+        Args:
+            node (QGNode): The node
+            value (str): The value of the child
+            
+        Returns:
+            int: The index of the child
+        """
+        for i, child in enumerate(node.parent.children):
+            if child.text == node.text:
+                return i
+        return -1
+    
     
     def build_query_str(self, node: QTNode):
         """
@@ -95,7 +115,7 @@ class TSQueryTree:
             curr_node = queue.pop(0)
             if not curr_node:
                 continue
-            graph.node(str(curr_node.id), str(curr_node.value) + " " + str(curr_node.child_index))
+            graph.node(str(curr_node.id), str(curr_node.value))
             if curr_node.parent:
                 graph.edge(str(curr_node.parent.id), str(curr_node.id))
             for child in curr_node.children:
@@ -103,16 +123,51 @@ class TSQueryTree:
         
         graph.render("./visualization/" + "query_tree")
 
-def test_query_tree():
-    tree = parse_js_code("func(a, b)")
-    ts_query_tree = TSQueryTree(tree.root_node)
-    ts_query_tree.build_query_tree(tree.root_node, ts_query_tree.root)
-    query_str = ts_query_tree.build_query_str(ts_query_tree.root)
-    ts_query_tree.visualize_query_tree()
-    print(query_str)
+    def handle_ellipsis(self, node: QTNode):
+        # traverse the tree
+        if node.text != None and len(node.text) > 39:
+            if node.text[39:] == "ELLIPSIS":
+                return True
+        for child in node.children:
+            if self.handle_ellipsis(child):
+                index = self.get_child_index_from_value(child)
+                for i in range(3):
+                    node.delete_child_index(index - 1) # delete the previous node then i becomes the previous and delete me and so on
+            else:
+                continue
+        return False
 
-test_query_tree()
-            
+#____________________________________________________________________________________#
+#                             MAIN FUCNTION IN CLASS
+#____________________________________________________________________________________#   
 
 
-        
+    def build_query_tree(self, node: Node, parent: QTNode):
+        """
+        This function builds the query tree from the TS_root.
+        """
+        curr_node = QTNode("(" + node.type, node.text.decode())
+        parent.add_child(curr_node)
+        parent = curr_node
+        for child in node.named_children:
+            if child == node.named_child(0):
+                parent.add_child(QTNode("."))
+            self.build_query_tree(child, curr_node)
+            parent.add_child(QTNode("."))
+        parent.add_child(QTNode(")"))
+
+        if(node.named_child_count == 0):
+            if(len(node.text.decode()) > 39):
+                if(node.text.decode()[39:] in self.catch and node.text.decode()[39:] != "_" and node.text.decode()[39:] != "ELLIPSIS"):
+                    if(self.catch[node.text.decode()[39:]] == node.text.decode()[1:39]):
+                        parent.add_child(QTNode("@param" + str(self.counter)))
+                        self.variables[self.counter] = node.text.decode()[39:]
+                        self.counter += 1
+                    else:
+                        parent.add_child(QTNode("@param" + str(self.counter)))
+                        self.content[self.counter] = node.text.decode()
+                        self.counter += 1 
+            else:
+                parent.add_child(QTNode("@param" + str(self.counter)))
+                self.content[self.counter] = node.text.decode()
+                self.counter += 1
