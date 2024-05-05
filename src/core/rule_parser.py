49288dc36421dedc9600ef1b2bd10434b32c6689
Anas Sherif <96656458+Femton02@users.noreply.github.com>
2024-05-05 17:41:44 +0300

    feat:Handles all cases of Ellipsis. feat: Replace for loops with a unique string in rule_parser.py
    This commit modifies the `rule_parser.py` module to replace for loops with a unique string. The for loops are identified using the pattern `for(<...>)` and replaced with a string that includes a unique identifier. This change improves the preprocessing of rules in the module.
    
    Co-authored-by: [Femton02]

--- src/core/rule_parser.py
+++ src/core/rule_parser.py
@@ -11,16 +11,22 @@ import re
 def rule_preprocessing(rule: str) -> tuple[str, dict]:
     variablepattern = r'(\$<(\w+)>)'        #pattern to catch variables
     ellipsispattern = r'(\$<(\.\.\.)>)'     #pattern to catch ellipsis
+    forpattern = r'for\((\$<\.\.\.>)\)'     #pattern to catch for loops
     catches = {}
     uuidobj = uuid.uuid4()
     uuidstr = str(uuidobj.int)[0:38] #to ensure fixed length
     for match in re.finditer(variablepattern, rule):
         catches[match.group(2)] = uuidstr
     outputstr = re.sub(variablepattern, "_"+ uuidstr +r'\2', rule)
+    
+    forstr = "for(_" + uuidstr + "FOR; _" + uuidstr + "FOR; _" + uuidstr + "FOR)"
+    outputstr = re.sub(forpattern, forstr, outputstr) #replace for loops with a unique string
 
     for match in re.finditer(ellipsispattern, outputstr):
         catches["ELLIPSIS"] = uuidstr
     outputstr = re.sub(ellipsispattern, "_"+ uuidstr + "ELLIPSIS", outputstr) #replace ellipsis with a unique string
+
+
     return outputstr, catches
 
 def get_first_multi_children_node(tree):
@@ -42,14 +48,15 @@ def parse_rule(rule: str) -> tuple[str, dict, dict]:
     root = get_first_multi_children_node(tree)
     tsqt = TSQueryTree(root, catch)
     tsqt.build_query_tree(root, tsqt.root)
-    tsqt.handle_ellipsis(tsqt.root)
+    tsqt.handle_ellipsis_and_for(tsqt.root)
     query = tsqt.build_query_str(tsqt.root) + "@content"
     variables = tsqt.variables
     content = tsqt.content
     tsqt.visualize_query_tree()
     return query, variables, content
 
-query, var, cont = parse_rule("func($<...>, $<zzz>, $<...>)")
+
+query, var, cont = parse_rule("const letters = new Set([$<...>, 1]);")
 print(query)
 print(var)
 print(cont)
--- src/core/tree_sitter_query_tree.py
+++ src/core/tree_sitter_query_tree.py
@@ -75,21 +75,6 @@ class TSQueryTree:
         self.variables = {}
         self.content = {}
 
-    def get_child_index_from_value(self, node: QTNode) -> int:
-        """
-        This function gets the index of the child with the given value.
-        
-        Args:
-            node (QGNode): The node
-            value (str): The value of the child
-            
-        Returns:
-            int: The index of the child
-        """
-        for i, child in enumerate(node.parent.children):
-            if child.text == node.text:
-                return i
-        return -1
     
     
     def build_query_str(self, node: QTNode):
@@ -128,14 +113,35 @@ class TSQueryTree:
         if node.text != None and len(node.text) > 39:
             if node.text[39:] == "ELLIPSIS":
                 return True
-        for child in node.children:
+        for index, child in enumerate(node.children):
             if self.handle_ellipsis(child):
-                index = self.get_child_index_from_value(child)
                 for i in range(3):
                     node.delete_child_index(index - 1) # delete the previous node then i becomes the previous and delete me and so on
+                if(node.value == "(field_definition" or node.value == "(expression_statement"):
+                    return True
             else:
                 continue
         return False
+    
+    def handle_for(self, node: QTNode):
+        if node.text != None and len(node.text) > 39:
+            if node.text[39:] == "FOR":
+                return True
+        for index, child in enumerate(node.children):
+            if self.handle_for(child):
+                if node.value == "(for_statement":
+                    node.children[index].value = "(_"
+                else:
+                    for i in range(3):
+                        node.delete_child_index(index -1)
+                    node.value = "(_"
+            else:
+                continue
+        return False
+    
+    def handle_ellipsis_and_for(self, node: QTNode):
+        self.handle_ellipsis(node)
+        self.handle_for(node)
 
 #____________________________________________________________________________________#
 #                             MAIN FUCNTION IN CLASS
