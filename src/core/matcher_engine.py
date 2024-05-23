a349ca271e58b8ff6da3bfec8fdb7be09b2b8762
Anas Sherif <96656458+Femton02@users.noreply.github.com>
2024-05-23 03:17:21 +0300

    feat. added trace focus in matcher engine

--- src/core/matcher_engine.py
+++ src/core/matcher_engine.py
@@ -46,11 +46,29 @@ def compare_variables(match: dict[str, Node], pattern: Pattern, helper_patterns:
                 helper_pattern_results = get_matches(node.text.decode(), helper_pattern.patterns, helper_patterns)
                 if not helper_pattern_results:
                     return False
-            
             # TODO: Handle other filter types
         
 
 
+    return True
+
+def trace_focus(focus: dict, match: dict[str, Node], parsed_src_code_tree: Tree):
+    comparestr = ""
+    for key, value in focus.items():
+        comparestr = "focus" + str(key)
+    if comparestr not in match:
+        return False
+    node = match[comparestr]
+    if value[1] == "LITERAL":
+        if node.text.decode() != value[1]:
+            return False
+        #look for the variable in the whole src code, then look for another operands in the same line and focus on them
+        varquery = "((identifier) @focus (#eq? @focus \""+value[0]+"\"))"
+        return True
+    else:
+        #look for the variable in the whole src code, then look for another operands in the same line and focus on them
+        varquery = "((identifier) @focus (#eq? @focus \""+node.text.decode()+"\"))"
+        return True
     return True
 
 def get_matches(src_code: str, patterns: list[Pattern], helper_patterns: list[HelperPattern]):
@@ -68,6 +86,9 @@ def get_matches(src_code: str, patterns: list[Pattern], helper_patterns: list[He
             if not compare_variables(match, pattern, helper_patterns):
                 # print(f"Type or variable Mismatch for match \n{match}\nand pattern \n{pattern}\n")
                 continue
+            if not trace_focus(pattern.focus, match, parsed_src_code_tree):
+                # print(f"Focus Mismatch for match \n{match}\nand pattern \n{pattern}\n")
+                continue
 
             # print(f"Found match: {match} for pattern {pattern}")
             result.append(match)
@@ -77,7 +98,7 @@ def get_matches(src_code: str, patterns: list[Pattern], helper_patterns: list[He
 
 
 if __name__ == "__main__":
-    rule = process_rule(protego_workspace_dir + "/test/rules/rule.yaml")
-    src_code = "{\ncookie: { httpOnly: false }\n}"
+    rule = process_rule(protego_workspace_dir + "/rules/rule.yaml")
+    src_code = "const html = `<div>${event.name}</div>`;\nconst someRandomStuff = {\n  // ok: tainted-html-response\n  data: event.foo\n}\nbar(someRandomStuff)\nvar zzz = html;\nconst response = {\n  statusCode: 200,\n  // ruleid: tainted-html-response\n  body: zzz,\n  headers: {\n    'Content-Type': 'text/html',\n  }\n};"
     matches = get_matches(src_code, rule.patterns, rule.helper_patterns)
-
+    print(f"Matches: {matches}")
